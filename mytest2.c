da20cfb09b477261a7d2f55cfcdc25f9c826e5d0
d3cker <pi@raspberrypi.home>
2016-08-27 15:17:18 +0000

    added led status

--- mytest2.c
+++ mytest2.c
@@ -14,6 +14,8 @@
 #include <string.h>
 #include <wiringPi.h>
 
+
+//LCD SetUp
 #define LCD_RS  22             //Register select pin
 #define LCD_E   17             //Enable Pin
 #define LCD_D4  18             //Data pin 4
@@ -21,10 +23,17 @@
 #define LCD_D6  24             //Data pin 6
 #define LCD_D7  23             //Data pin 7
 
-const int butPin = 2;
+//Switches and LEDs SetUp
+#define BUTPINA 21
+#define BUTPINB 20
+#define BLUELED 12
+#define REDLED 16
 
 int lcd; //global LCD handler
-
+int RedStatus;   //RED LED Status
+int BlueStatus;  //BLUE LED status
+int RS_Status;   //RED switch status
+int BS_Status;   //BLUE switch status
 
 void initLCD() {
   if (lcd = lcdInit (2,16,4,LCD_RS,LCD_E,LCD_D4,LCD_D5,LCD_D6,LCD_D7,0,0,0,0)){
@@ -33,19 +42,53 @@ void initLCD() {
    }
 }
 
-void main(int argc, char **argv) {
+void LedsOff() {
+    RedStatus = LOW;
+    BlueStatus = LOW;
+}
+
+void initMyPi() {
     wiringPiSetupGpio();
     initLCD();
     lcdClear(lcd);
-    pinMode(butPin,INPUT);
-    pullUpDnControl(butPin, PUD_UP);
+    pinMode(BUTPINA,INPUT);
+    pinMode(BUTPINB,INPUT);
+    pinMode(BLUELED,OUTPUT);
+    pinMode(REDLED,OUTPUT);
+    pullUpDnControl(BUTPINA, PUD_UP);
+    pullUpDnControl(BUTPINB, PUD_UP);
+    LedsOff();
+}
+
+void main(int argc, char **argv) {
+    initMyPi();
     while(1) {
-	lcdPosition(lcd,0,1);
-	if(digitalRead(butPin)) {
-	    lcdPuts(lcd,"::: OFF :::");
+	lcdPosition(lcd,0,0);
+	if(digitalRead(BUTPINB)) {
+	    lcdPuts(lcd,"RS: OFF :: RL: ");
+	    RS_Status = 0;
 	} else {
-	    lcdPuts(lcd,"::: ON  :::");
+	    lcdPuts(lcd,"RS: ON  :: RL: ");
+	    if (!RS_Status) {
+		RedStatus = !RedStatus;
+		RS_Status = !RS_Status;
+	    }
 	}
-//should we sleep here for a while ?
+	lcdPrintf(lcd,"%d",RedStatus);
+	lcdPosition(lcd,0,1);
+	if(digitalRead(BUTPINA)) {
+	    lcdPuts(lcd,"BS: OFF :: BL: ");
+	    BS_Status = 0;
+	} else {
+	    lcdPuts(lcd,"BS: ON  :: BL: ");
+	    if (!BS_Status) {
+		BlueStatus = !BlueStatus;
+		BS_Status = !BS_Status;
+	    }
+	}
+	lcdPrintf(lcd,"%d",BlueStatus);
+	//should we sleep here for a while ?
+	digitalWrite(REDLED, RedStatus);
+	digitalWrite(BLUELED, BlueStatus);
     }
 }
