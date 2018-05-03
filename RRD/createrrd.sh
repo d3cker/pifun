d5764ade38d2cbf8f494cbb6e4a7e474f7e4e232
d3cker <l33tdev.marcinkowski@gmail.com>
2018-05-03 16:01:12 +0000

    added rrd create script

---
+++ RRD/createrrd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+mkdir /rrd
+
+rrdtool create /rrd/datahumi.rrd \
+--step '5' \
+'DS:humi:GAUGE:60:0:100' \
+'RRA:MIN:0.5:1:2000' \
+'RRA:AVERAGE:0.5:1:2000' \
+'RRA:MAX:0.5:1:2000'
+
+rrdtool create /rrd/datatemp.rrd \
+--step '5' \
+'DS:temp:GAUGE:60:-100:100' \
+'RRA:MIN:0.5:1:2000' \
+'RRA:AVERAGE:0.5:1:2000' \
+'RRA:MAX:0.5:1:2000'
